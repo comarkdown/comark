1026f45fbecd962f8dafe7e0f5c02c6518c8916a
Farnabaz <farnabaz@gmail.com>
2026-02-12 12:25:24 +0100

    docs: add favicon

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,8 +2,20 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   extends: ['docus'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', href: '/icon.svg', type: 'image/svg+xml' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+      ],
+    },
+  },
   css: ['~/assets/styles/main.css'],
-  // @ts-expect-error - Nuxt Content types are not loaded
+  // @ts-expect-error - Nuxt SEO types are not loaded
+  site: {
+    url: 'https://mdc-syntax.vercel.app',
+  },
   content: {
     experimental: {
       sqliteConnector: 'native',
