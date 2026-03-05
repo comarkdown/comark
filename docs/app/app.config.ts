c5718a181b295aaed947ab94e286ae0f962cebbd
Elliott Johnson <hello@ell.iott.dev>
2026-03-05 10:23:59 -0700

    docs

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   seo: {
     title: 'Comark',
-    description: 'Components in Markdown (Comark) parser with streaming support for Vue and React.',
+    description: 'Components in Markdown (Comark) parser with streaming support for Vue, React and Svelte.',
     url: 'https://comark.dev',
     socials: {
       github: 'comarkdown/comark',
@@ -12,7 +12,7 @@ export default defineAppConfig({
   },
 
   title: 'Comark',
-  description: 'Components in Markdown (Comark) parser with streaming support for Vue and React.',
+  description: 'Components in Markdown (Comark) parser with streaming support for Vue, React and Svelte.',
   url: 'https://comark.dev',
 
   ui: {
@@ -32,6 +32,7 @@ export default defineAppConfig({
         'md': 'i-custom-comark',
         'react': 'i-logos-react',
         'html': 'i-vscode-icons-file-type-html',
+        'svelte': 'i-simple-icons-svelte',
       },
     },
   },
