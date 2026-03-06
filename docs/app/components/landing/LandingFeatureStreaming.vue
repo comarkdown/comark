60d568b8fc917d000612f8e068ddd7f021fea9bf
Sébastien Chopin <atinux@gmail.com>
2026-03-06 09:05:27 +0100

    docs: update

--- docs/app/components/landing/LandingFeatureStreaming.vue
+++ docs/app/components/landing/LandingFeatureStreaming.vue
@@ -7,13 +7,13 @@ defineProps<{
   linkTo: string
 }>()
 
-const STREAMING_MD = `Awake! for Morning in the Bowl of Night
+const STREAMING_MD = `Awake! for **Morning in the Bowl of Night**
 Has flung the Stone that puts the Stars to Flight:
-And Lo! the Hunter of the East has caught
+And Lo! the \`Hunter of the East\` has caught
 The Sultan's Turret in a Noose of Light.
 
 Come, fill the Cup, and in the fire of Spring
-The Winter Garment of Repentance fling:
+_The Winter Garment of Repentance fling_:
 The Bird of Time has but a little way
 To flutter—and the Bird is on the Wing.`
 
