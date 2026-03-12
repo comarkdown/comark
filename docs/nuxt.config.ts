fb2297b67d48d5e8ccf35d4ff1ee88fd59f3f7aa
Sébastien Chopin <atinux@gmail.com>
2026-03-12 14:13:13 +0100

    docs: update vercel analytic & speed insights deps

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   extends: ['docus'],
-  modules: ['nuxt-studio', '@comark/nuxt'],
+  modules: [
+    'nuxt-studio',
+    '@comark/nuxt',
+    '@vercel/speed-insights',
+    '@vercel/analytics',
+  ],
 
   app: {
     head: {
@@ -12,6 +17,10 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
       ],
     },
+  },
+
+  build: {
+    transpile: ['comark', '@comark/vue'],
   },
 
   css: ['~/assets/styles/main.css'],
