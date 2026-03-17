a8f577ac6e9260a9247dcba20f4116a854f18f62
Farnabaz <farnabaz@gmail.com>
2026-03-17 15:19:46 +0100

    up

--- examples/1.frameworks/nuxt/nuxt.config.ts
+++ examples/1.frameworks/nuxt/nuxt.config.ts
@@ -3,9 +3,10 @@ export default defineNuxtConfig({
   modules: ['@comark/nuxt', '@nuxt/ui'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  routeRules: {
-    '/': { prerender: true },
-    '/blog/**': { prerender: true },
-  },
   compatibilityDate: '2025-07-15',
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
+  },
 })
