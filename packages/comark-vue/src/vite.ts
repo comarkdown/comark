0d2fb8a7f411b49b1fc70c51185644e0f629b0df
Arash Ari Sheyda <hi@arashsheyda.me>
2026-04-09 21:34:45 -0600

    feat: add vite devtolls integration with playground

--- packages/comark-vue/src/vite.ts
+++ packages/comark-vue/src/vite.ts
@@ -196,5 +196,16 @@ export default function comark(): Plugin {
       server.watcher.on('add', invalidate)
       server.watcher.on('unlink', invalidate)
     },
+    devtools: {
+      setup(ctx) {
+        ctx.docks.register({
+          id: 'comark',
+          title: 'Comark',
+          icon: `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212 212"><path stroke="currentColor" stroke-width="8" fill="none" d="M200.4 52.5v110H10.4v-110h190z"/><path fill="currentColor" d="M129.4 94.8V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zm30.1-44.7V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zM31.4 141.5v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39h-20z"/></svg>')}`,
+          type: 'iframe',
+          url: 'https://comark.dev/play',
+        })
+      },
+    },
   }
 }
