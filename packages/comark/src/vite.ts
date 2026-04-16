ca98ddb136f2ad0c0076e211c10eb6c23c5b37d0
Arash Ari Sheyda <hi@arashsheyda.me>
2026-04-15 21:37:30 -0600

    fix: address pr suggestion

--- packages/comark/src/vite.ts
+++ packages/comark/src/vite.ts
@@ -1,8 +1,7 @@
-/// <reference types="@vitejs/devtools-kit" />
-
 import type { Plugin } from 'vite'
 
-const COMARK_ICON = `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212 212"><path stroke="currentColor" stroke-width="8" fill="none" d="M200.4 52.5v110H10.4v-110h190z"/><path fill="currentColor" d="M129.4 94.8V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zm30.1-44.7V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zM31.4 141.5v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39h-20z"/></svg>')}`
+const COMARK_LIGHT_ICON = `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212 212"><path stroke="#000" stroke-width="8" fill="none" d="M200.4 52.5v110H10.4v-110h190z"/><path fill="#000" d="M129.4 94.8V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zm30.1-44.7V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zM31.4 141.5v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39h-20z"/></svg>')}`
+const COMARK_DARK_ICON = `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212 212"><path stroke="#fff" stroke-width="8" fill="none" d="M200.4 52.5v110H10.4v-110h190z"/><path fill="#fff" d="M129.4 94.8V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zm30.1-44.7V75.5h19.9v19.3h-19.9zm0 44.7v-19.2h19.9v19.2h-19.9zM31.4 141.5v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39h-20z"/></svg>')}`
 
 /**
  * Vite plugin that registers the Comark playground in Vite DevTools.
@@ -28,7 +27,10 @@ export function comarkDevtools(): Plugin {
         ctx.docks.register({
           id: 'comark',
           title: 'Comark',
-          icon: COMARK_ICON,
+          icon: {
+            light: COMARK_LIGHT_ICON,
+            dark: COMARK_DARK_ICON,
+          },
           type: 'iframe',
           url: 'https://comark.dev/play',
         })
