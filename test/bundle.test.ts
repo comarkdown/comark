a4497920ac76a99e28eca8b020596f34b4a4b59b
Farnabaz <farnabaz@gmail.com>
2026-07-30 15:38:23 +0200

    Update bundle.test.ts

--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -67,7 +67,7 @@ describe('package bundle size', { timeout: 60_000 }, () => {
         "@comark/react": "40.5k (58 files)",
         "@comark/svelte": "40.6k (66 files)",
         "@comark/vue": "57.3k (62 files)",
-        "comark": "372k (136 files)",
+        "comark": "374k (136 files",
       }
     `)
   })
