e1ddc510a41693dd3f99617cffe957c8fa6435f8
Farnabaz <farnabaz@gmail.com>
2026-07-30 17:30:36 +0200

    fix: attributes can start with `[:a-z_]`

--- packages/comark/src/internal/parse/syntax/props.ts
+++ packages/comark/src/internal/parse/syntax/props.ts
@@ -52,11 +52,15 @@ export function searchProps(content: string, index = 0) {
       const char = content[index]
       if (start !== index) {
         const key = content.slice(start, index).trim()
+        let value = ''
         if (char === '=') {
           index += 1
-          props.push([key, searchValue()])
-        } else if (key.match(/^[a-z0-9_\-:]+$/gi)) {
-          props.push([key, 'true'])
+          value = searchValue()
+        } else {
+          value = 'true'
+        }
+        if (key.match(/^:?[a-z_][a-z0-9_-]+$/gi)) {
+          props.push([key, value])
         }
       }
     }
