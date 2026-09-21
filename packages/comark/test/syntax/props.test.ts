7a2761d796d5afbeae3210651c0b212d9d256ce9
David <dbzx-10299@hotmail.com>
2026-09-21 16:42:56 +0200

    test(props): cover tailwind custom class values

--- packages/comark/test/syntax/props.test.ts
+++ packages/comark/test/syntax/props.test.ts
@@ -71,4 +71,35 @@ describe('parseProps', () => {
       "
     `)
   })
+
+  it('tailwind custom values', () => {
+    expect(parseMarkdown('{.bg-[#000]}')).toMatchInlineSnapshot(`
+      "
+      class=bg-[#000]
+      "
+    `)
+
+    // decimal `.` inside brackets must not be treated as a new class
+    expect(parseMarkdown('{.text-[1.5rem]}')).toMatchInlineSnapshot(`
+      "
+      class=text-[1.5rem]
+      "
+    `)
+
+    // nested brackets, e.g. an arbitrary calc() value
+    expect(parseMarkdown('{.w-[calc(100%-1rem)]}')).toMatchInlineSnapshot(`
+      "
+      class=w-[calc(100%-1rem)]
+      "
+    `)
+
+    // multiple custom-value classes plus a trailing id
+    expect(parseMarkdown('{.bg-[#000] .text-[14px] #my-id}')).toMatchInlineSnapshot(`
+      "
+      class=bg-[#000]
+      class=text-[14px]
+      id=my-id
+      "
+    `)
+  })
 })
